4c971d9f5c5d85cb3b11c0e2f72fea7fd16bfcd7
Wisyle <Robert5560newton@gmail.com>
2026-04-17 09:45:06 +0100

    Add Koala tracking script

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="vite/client" />
 
+interface Window {
+  umami?: {
+    track: (event: string, data?: Record<string, unknown>) => void;
+  };
+}
+
 interface ImportMetaEnv {
   readonly VITE_API_URL?: string
 }
